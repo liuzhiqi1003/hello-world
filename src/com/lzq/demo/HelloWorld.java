8448c85b0dfea3a4fd24bcc4a2d5d49b3a6b6148
liuzhiqi <liuzhiqi@zrcaifu.com>
2016-05-19 14:35:42 +0800

    Initial commit

---
+++ src/com/lzq/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.lzq.demo;
+
+/**
+ * Created by liuzhiqi on 16/5/19.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
